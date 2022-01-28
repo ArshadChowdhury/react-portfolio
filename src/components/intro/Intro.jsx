547b73764302fb230504938d04098c571eb28870
Arshad Chowdhury <arshadchowdhury46@gmail.com>
2022-01-28 18:12:36 +0600

    Intro part done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,37 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
-export default function intro() {
+export default function Intro() {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
-      z
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/arshad_pic.png" alt="Arshad" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There !! I'm</h2>
+          <h1>Arshad</h1>
+          <h3>
+            A Web <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down_arrow.jpg" alt="Down Arrow" />
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,23 @@ import "./portfolio.scss";
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      d
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://image.freepik.com/free-vector/banking-app-interface-screens_23-2148604478.jpg"
+            alt="Banking App Picture"
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   );
 }
