69426bf7ce4d8beecf579a82ca81aeb54f805f7b
Arshad Chowdhury <arshadchowdhury46@gmail.com>
2022-02-05 22:01:53 +0600

    working on portfolio section now updated some of it

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,49 +1,70 @@
+import Portfoliolist from "../portfolioList/Portfoliolist";
 import "./portfolio.scss";
+import { useEffect, useState } from "react";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    { id: "featured", title: "Featured" },
+    { id: "web", title: "Web App" },
+    { id: "mobile", title: "Mobile App" },
+    { id: "design", title: "Design" },
+    { id: "content", title: "Content" },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+
+      default:
+        setData(featuredPortfolio);
+        break;
+    }
+  }, [selected]);
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">Featured</li>
-        <li>Web App</li>
-        <li>Mobile App</li>
-        <li>Design</li>
-        <li>Branding</li>
+        {list.map((item, i) => (
+          <Portfoliolist
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+            key={i}
+          />
+        ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img src="assets/project.jpg" alt="Banking App Picture" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/project.jpg" alt="Banking App Picture" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/project.jpg" alt="Banking App Picture" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/project.jpg" alt="Banking App Picture" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/project.jpg" alt="Banking App Picture" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/project.jpg" alt="Banking App Picture" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/project.jpg" alt="Banking App Picture" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/project.jpg" alt="Banking App Picture" />
-          <h3>Banking App</h3>
-        </div>
+        {data.map((d) => (
+          <div className="item">
+            <img src={d.img} alt="Banking App Picture" />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,30 @@ import "./works.scss";
 export default function works() {
   return (
     <div className="works" id="works">
-      a
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7nsuzWIJLjWUj9iwwx5vLNLAwOZ-WL4KGOg&usqp=CAUU"
+                    alt="Project"
+                  />
+                </div>
+                <h2>Title</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Voluptates cupiditate laboriosam maxime repudiandae expedita
+                  sequi enim optio quasi vel quas?
+                </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
